072addaa3c4965a622c7b984e4697605d579bd54
OlawaleJoseph <adedekojoseph@gmail.com>
2020-07-14 15:53:38 +0100

    added draw game logic

--- bin/main.rb
+++ bin/main.rb
@@ -1,10 +1,8 @@
 #!/usr/bin/env ruby
 require './lib/app'
 
-game = Game.new
-
 def get_input(player)
-  puts "Player #{player}'s Turn"
+  puts "Player #{player.player_number}'s Turn (#{player.player_tag})"
   user_input = gets.chomp.to_i
   loop do
     return user_input if (user_input.is_a? Integer) && (user_input.positive? && user_input < 10)
@@ -14,31 +12,48 @@ def get_input(player)
   end
 end
 
-puts 'Gameplay, Player 1 should choose his/her tag, player 2 gets assigned a tag automatically.
-  To place your tag on the board, choose a number
-  between 1-9 as displayed in the board above. The first player who gets three(3) tags in vertically,
-  horizontally or diagonally lined up wins!'
-puts '-------------------------'
-puts '|   1   |   2   |   3   |'
-puts '-------------------------'
-puts '|   4   |   5   |   6   |'
-puts '-------------------------'
-puts '|   7   |   8   |   9   |'
-puts '-------------------------'
+def play
+  puts 'Gameplay, Player 1 should choose his/her tag, player 2 gets assigned a tag automatically.
+    To place your tag on the board, choose a number
+    between 1-9 as displayed in the board above. The first player who gets three(3) tags in vertically,
+    horizontally or diagonally lined up wins!'
+  puts '-------------------------'
+  puts '|   1   |   2   |   3   |'
+  puts '-------------------------'
+  puts '|   4   |   5   |   6   |'
+  puts '-------------------------'
+  puts '|   7   |   8   |   9   |'
+  puts '-------------------------'
 
-puts 'Enter Numbers Between 1 - 9'
-board = Board.new
+  puts 'Enter Numbers Between 1 - 9'
 
-current_player_input = get_input(game.player1.player_number)
-count = 1
-until game.check_win?(board.board) || game.check_draw?(board.board)
+  game = Game.new
+  board = Board.new
+  current_player = nil
+  count = 1
+  current_player_input = get_input(game.player1)
+  until game.check_win?(board.board) || game.check_draw?(board.board)
 
-  until board.valid_position?(current_player_input)
-    current_player_input = count.odd? ? get_input(game.player1.player_number) : get_input(game.player2.player_number)
+    current_player_input = count.odd? ? get_input(game.player1) : get_input(game.player2) until board.valid_position?(current_player_input)
+    current_player = count.odd? ? game.player1 : game.player2
+    board.update_board(current_player_input, current_player)
+    puts board.display_board
+    count += 1
   end
-  current_player = count.odd? ? game.player1 : game.player2
-  board.update_board(current_player_input, current_player)
-  board.display_board
-  # Check if Player has won
-  count += 1
+
+  if game.check_win?(board.board)
+    puts "Player #{current_player.player_number} (#{current_player.player_tag}) WON!!!"
+
+    puts 'Do you want a re-match? Y for YES'
+    replay = gets.chomp
+    board.reset_board
+    play if replay == 'y'
+  elsif game.check_draw?(board.board)
+    puts 'DRAW GAME. TRY AGAIN?!! Y for YES'
+    replay = gets.chomp
+    board.reset_board
+    play if replay == 'y'
+  end
 end
+
+play
--- lib/app.rb
+++ lib/app.rb
@@ -25,30 +25,32 @@ class Game
   def check_win?(board)
     new_array = []
     new_array.push(board[0], board[3], board[6])
-    return true if new_array.all?('X')
+    return true if new_array.all?('X') || new_array.all?('O')
 
     new_array = []
     new_array.push(board[1], board[4], board[7])
-    return true if new_array.all?('X')
+    return true if new_array.all?('X') || new_array.all?('O')
 
     new_array = []
     new_array.push(board[2], board[5], board[8])
-    return true if new_array.all?('X')
+    return true if new_array.all?('X') || new_array.all?('O')
 
     new_array = []
     new_array.push(board[0], board[4], board[8])
-    return true if new_array.all?('X')
+    return true if new_array.all?('X') || new_array.all?('O')
 
     new_array = []
     new_array.push(board[2], board[4], board[6])
-    return true if new_array.all?('X')
-    return true if board[0..2].all?('X') || board[3..5].all?('X') || board[6..8].all?('X')
+    return true if new_array.all?('X') || new_array.all?('O')
+    if (board[0..2].all?('X') || board[0..2].all?('O')) || (board[3..5].all?('X') || board[3..5].all?('O')) || (board[6..8].all?('X') || board[6..8].all?('O'))
+      return true
+    end
 
     false
   end
 
   def check_draw?(board)
-    false unless check_win?(board) || @board.board.none?(Integer)
+    !check_win?(board) && board.none?(Integer) ? true : false
   end
 end
 
@@ -71,10 +73,12 @@ class Board
   end
 
   def display_board
+    board_string = ''
     @board.each_with_index do |val, i|
-      print "#{val} "
-      puts '' if i == 2 || i == 5 || i == 8
+      board_string << "#{val} "
+      board_string << "\n" if i == 2 || i == 5 || i == 8
     end
+    board_string
   end
 
   def valid_position?(position)
